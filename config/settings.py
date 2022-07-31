89e12b1d1eb8430bfc4920de4f08f6e923a02b29
Zhanat <janat.200066@gmail.com>
2022-07-31 20:03:56 +0600

    pre_end project

--- config/settings.py
+++ config/settings.py
@@ -65,12 +65,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db", 
+        "PORT": 5432,  
     }
 }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
